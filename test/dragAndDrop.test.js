b8ed8aeea42ba2bd87eddcb3f5f3b229a1454272
solarjoni <solarjoni@gmail.com>
2022-01-03 18:23:33 +0200

    Drag and Drop

---
+++ test/dragAndDrop.test.js
@@ -0,0 +1,18 @@
+const { assert } = require("chai");
+const internetPage = require("./pageobjects/internet.page");
+
+describe("Drag and Drop", async () => {
+    it.skip("should drag from box A to box B, not working", async () => {
+        await browser.url(`${browser.options.baseUrl}/drag_and_drop`);
+        await internetPage.dragColumnAToColumnB();
+        await browser.pause(5000);
+        assert.equal(await internetPage.columnBHeader.getText(), 'A');
+    });
+
+    it("should drag and drop", async () => {
+        await browser.url('https://crossbrowsertesting.github.io/drag-and-drop.html');
+        await internetPage.dragDraggableToDroppable();
+        //await browser.pause(5000);
+        assert.equal(await internetPage.droppableParagraph.getText(), 'Dropped!');
+    });
+})
