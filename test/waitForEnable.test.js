acc2e6bf526349d3152e6c9b9a7c7677ca2e37b0
solarjoni <solarjoni@gmail.com>
2022-01-04 11:39:21 +0200

    waitForEnabled and browser.pause

---
+++ test/waitForEnable.test.js
@@ -0,0 +1,17 @@
+const { assert } = require("chai");
+const internetPage = require("./pageobjects/internet.page");
+
+describe("Wait for Enabled/Disabled", async () => {
+    it("should wait for the input field to be enabled", async () => {
+        await browser.url(`${browser.options.baseUrl}/dynamic_controls`);
+        await internetPage.clickEnableButton();
+        await internetPage.inputEnabledField.waitForEnabled(4000);
+        assert.equal(await internetPage.inputEnabledField.isEnabled(), true);
+    });
+
+    it("should wait for the input field to be disabled", async () => {
+        await internetPage.clickEnableButton();
+        await internetPage.inputEnabledField.waitForEnabled( {timeout: 4000, reverse: true} );
+        assert.equal(await internetPage.inputEnabledField.isEnabled(), false);
+    });
+})
